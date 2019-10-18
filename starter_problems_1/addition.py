0f406a39c59e7fa0daf3b704b266e960fc3646f2
Matthew Cho <56496095+mattcho1157@users.noreply.github.com>
2019-10-18 13:19:26 +1000

    Update addition.py

--- starter_problems_1/addition.py
+++ starter_problems_1/addition.py
@@ -1,1 +1,17 @@
+# Reading the file
+f = open("addin.txt", "r")
+numbers = f.readline()
+f.close()
 
+# Spliting the two numbers
+values = numbers.split(" ")
+number_1 = int(values[0])
+number_2 = int(values[1])
+
+#Adding the numbers
+added = int((number_1) + (number_2))
+
+# Writing to a file
+f = open("addout.txt","w")
+f.write(str(added) + "\n")
+f.close()
