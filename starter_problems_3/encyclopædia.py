5bad4f904e7d91137d1a0ab2cea5406d48037fa8
Matthew Cho <56496095+mattcho1157@users.noreply.github.com>
2019-10-18 14:31:26 +1000

    Create encyclopædia.py

---
+++ starter_problems_3/encyclopædia.py
@@ -0,0 +1,20 @@
+f = open("encyin.txt","r")
+
+firstLine = f.readline().split(" ")
+words = int(firstLine[0])
+allLines = f.readlines()
+
+wordList = allLines[0:words]
+questionList = allLines[words:]
+
+f.close()
+
+f = open("encyout.txt","w")
+
+for page in questionList:
+
+    page = int(page)
+    answer = int(wordList[page-1])
+    f.write(str(answer) + "\n")
+
+f.close()
